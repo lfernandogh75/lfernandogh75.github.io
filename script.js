677cdb5335646412c3c1c155fa726264da35eee8
lfernandogh75 <fernandogh75@misena.edu.co>
2025-06-25 11:33:22 -0500

    cambio en pdf

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-function dowloadPDF(){
-    
-    // code to download pdf
-    // you can use the following code to download the pdf
-    const element = document.querySelector('container-lg');
-    const opt = {
-        margin: 0,
-        filename: 'Hoja_de_vida_Luis_Gallegpo.pdf',
-        image: { type: 'jpeg', quality: 1,scroll:0 },
-        html2canvas: { scale: 2 , useCors:true},
-        jsPDF: { unit: 'mm', format: 'letter', orientation: 'portrait' },
-        pagebreak: { mode: ['avoid-all', 'css', 'legacy'] },
-        enableLinks: true,
-    };
-    html2pdf()
-        .from(element)
-        .set(opt)
-        .save();
+function downloadPDF() {
+  // code to download pdf
+  // you can use the following code to download the pdf
+  const element = document.querySelector("container-lg");
+  const opt = {
+    margin: 0,
+    filename: "Hoja_de_vida_Luis_Gallegpo.pdf",
+    image: { type: "jpeg", quality: 1, scroll: 0 },
+    html2canvas: { scale: 2, useCors: true },
+    jsPDF: { unit: "mm", format: "letter", orientation: "portrait" },
+    pagebreak: { mode: ["avoid-all", "css", "legacy"] },
+    enableLinks: true,
+  };
+  html2pdf().set(opt).from(element).save();
 }
